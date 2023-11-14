ada8738aeac13f21785f0e61a09fef2da64384ba
Seyma <63148309+seymagkts@users.noreply.github.com>
2023-11-14 14:49:58 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,158 +1,151 @@
-"""
-Financial modeling
-"""
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-import statsmodels.api as sm
+import warnings
+from math import log
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import cross_val_score
+
+df = pd.read_excel("dataset_tupras.xlsx")
+
+warnings.simplefilter(action="ignore")
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_rows', 20)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+# data info
+def check(dataframe,head=5):
+    print(dataframe.shape)
+    print("********************************************************************")
+    print(dataframe.dtypes)
+    print("********************************************************************")
+    print(dataframe.info())
+    print("********************************************************************")
+    print(dataframe.describe().T)
+    print("********************************************************************")
+    print(dataframe.columns)
+    print("********************************************************************")
+    print(dataframe.head(head))
+    print("********************************************************************")
+    print(dataframe.tail(head))
+    print("********************************************************************")
+    print(dataframe.isnull().sum())
+    print("********************************************************************")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+##  Numerik ve kategorik değişken yakalanması
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+    return cat_cols, num_cols, cat_but_car
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+## Numerik değişken analizi
+## sayısal degiskenlerin çeyreklik değerlerini gösterir ve histogram grafiği oluşturur
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist(bins=20)
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show(block=True)
+
+for col in num_cols:
+    num_summary(df,col,True)
+
+## ozellik cikarimi
+def return_value(df,col_name):
+    i = 0
+    val_arr = [0]
+    for num in range(0,len(df)-1):
+        val = (-1)*log(df[col_name][i]/df[col_name][i+1])
+        i += 1
+        val_arr.append(val)
+    return val_arr
+
+def return_r_value(df,col_name2,col_name1="RFAIZ"):
+    i = 0
+    val_arr = []
+    for num in range(0,len(df)):
+        val =(df[col_name2][i] - df[col_name1][i])*100
+        i += 1
+        val_arr.append(val)
+    return val_arr
+
+df["RTUPRS"] = return_value(df,"TUPRS")
+df["RBIST100"] = return_value(df,"BIST100")
+df["RKUR"] = return_value(df,"USD/TRY")
+df["RPETROL"] = return_value(df,"PETROL")
+df["RFAIZ"] = return_value(df,"FAIZ")
+df["RSUE"] = return_value(df,"SUE")
+df["RALTIN"] = return_value(df,"ALTIN")
+df["RM2"] = return_value(df,"M2")
+df["RM3"] = return_value(df,"M3")
+df["ENF"] = return_value(df,"TUFE")
+df["ERTUPRS"] = return_r_value(df,"RTUPRS")
+df["ERBIST100"] = return_r_value(df,"RBIST100")
 
 
-train_data = pd.read_excel("train.xlsx")
-td = pd.read_excel("test.xlsx")
-test_data = pd.DataFrame(td)
-dep_var = train_data[["ERTUPRS"]]
-indep_var = train_data[["ERBIST100",
-                        "RKUR",
-                        "RPETROL",
-                        "RFAIZ",
-                        "RSUE",
-                        "RALTIN",
-                        "RM2",
-                        "RM3",
-                        "RTUFE"]]
+## Model
+y = df[["ERTUPRS"]]
+X = df[["ERBIST100",
+        "RKUR",
+        "RPETROL",
+        "RFAIZ",
+        "RSUE",
+        "RALTIN",
+        "RM2",
+        "RM3",
+        "ENF"]]
 
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.1,random_state=1)
+reg_model = LinearRegression().fit(X_train,y_train)
 
-class Analysis:
-    """
-    Necessary for analysis
-    """
-    def read_value(self,arr,txt, dataset):
-        """
-        Read excel
-        """
-        arr_ = dataset[txt].values
-        for i in arr_:
-            arr.append(i)
+print(reg_model.intercept_)
+print(reg_model.coef_)
 
-    def test_model(self,dict_model):
-        """
-        Model equation
-        """
-        return (model.intercept_) + (
-                model.coef_[0][0]*dict_model["bist100"]) + (
-                model.coef_[0][1]*dict_model["kur"]) + (
-                model.coef_[0][2]*dict_model["petrol"]) + (
-                model.coef_[0][3]*dict_model["faiz"]) + (
-                model.coef_[0][4]*dict_model["sue"]) + (
-                model.coef_[0][5]*dict_model["altin"]) + (
-                model.coef_[0][6]*dict_model["arzm2"]) + (
-                model.coef_[0][7]*dict_model["arzm3"]) + (
-                model.coef_[0][8]*dict_model["tufe"])
+# train RMSE
+y_pred = reg_model.predict(X_train)
+print("train RMSE ",np.sqrt(mean_squared_error(y_train,y_pred)))
 
-class Model:
-    """
-    Creating two different models
-    """
-    def stats_model(self, dependent, independent):
-        """
-        For Stats model
-        """
-        const = sm.add_constant(independent)
-        model_arb = sm.OLS(dependent, const).fit()
-        return model_arb.summary()
+# train RKARE
+print("train RKARE ",reg_model.score(X_train,y_train))
 
-    def sk_model(self, dependent, independent):
-        """"
-        For Scikit model
-        """
-        linear_reg = LinearRegression()
-        return linear_reg.fit(independent, dependent)
+# test RMSE
+y_pred = reg_model.predict(X_test)
+print("test RMSE ",np.sqrt(mean_squared_error(y_test,y_pred)))
 
-model_obj = Model()
-analysis_obj = Analysis()
+# test RKARE
+print("test RKARE ",reg_model.score(X_test,y_test))
 
-model = model_obj.sk_model(dep_var,indep_var)
+# 10 katlı CV RMSE
+print("10 katlı CV RMSE ",np.mean(np.sqrt(-cross_val_score(reg_model,X,y,cv=10,scoring="neg_mean_squared_error"))))
 
-TUPRS_values = []
-BIST100_values = []
-kur_values = []
-petrol_values = []
-altin_values = []
-tufe_values = []
-m2_values = []
-m3_values = []
-sue_values = []
-faiz_values = []
+# 5 katlı CV RMSE
+print("5 katlı CV RMSE ",np.mean(np.sqrt(-cross_val_score(reg_model,X,y,cv=5,scoring="neg_mean_squared_error"))))
 
-
-analysis_obj.read_value(TUPRS_values, "ERTUPRS", test_data)
-analysis_obj.read_value(BIST100_values, "ERBIST100", test_data)
-analysis_obj.read_value(kur_values, "RKUR", test_data)
-analysis_obj.read_value(petrol_values, "RPETROL", test_data)
-analysis_obj.read_value(altin_values, "RALTIN", test_data)
-analysis_obj.read_value(tufe_values, "RTUFE", test_data)
-analysis_obj.read_value(m2_values, "RM2", test_data)
-analysis_obj.read_value(m3_values, "RM3", test_data)
-analysis_obj.read_value(sue_values, "RSUE", test_data)
-analysis_obj.read_value(faiz_values, "RFAIZ", test_data)
-
-COUNT = len(BIST100_values)
-arr_result = []
-
-for index in range(COUNT):
-    func_param = {"bist100": BIST100_values[index],
-            "kur": kur_values[index],
-            "petrol": petrol_values[index],
-            "faiz": faiz_values[index],
-            "sue":sue_values[index],
-            "altin":altin_values[index],
-            "arzm2":m2_values[index],
-            "arzm3":m3_values[index],
-            "tufe" :tufe_values[index]}
-    test_values = analysis_obj.test_model(func_param)
-    arr_result.append(test_values)
-    func_param.clear()
-
-# Graphic
-class Visualize:
-    """
-    Model and variable visualization
-    """
-    def line_vis(self,result,real):
-        """
-        Line plot
-        """
-        plt.figure(figsize=(11, 8))
-        plt.style.use("classic")
-        plt.plot(result,
-                 color="#cd3333",
-                 linewidth=3,
-                 linestyle="--",
-                 marker="o",
-                 markersize=8,
-                 markerfacecolor="yellow",
-                 markeredgewidth=2,
-                 label="Model return")
-        plt.plot(real,
-                 "#3d59ab",
-                 linewidth=3,
-                 linestyle="--",
-                 marker="o",
-                 markersize=8,
-                 label="Real return")
-        plt.ylabel("Model")
-        plt.xlabel("Real")
-        plt.legend(shadow=True)
-        plt.savefig(quality=95,fname="chart.png",facecolor="white")
-        plt.show()
-
-    def cor_vis(self,data):
-        """
-        Correlation matrix
-        """
-        sns.heatmap(data.corr(), annot= True)
-
-
-vis = Visualize()
-vis.line_vis(arr_result,TUPRS_values)
